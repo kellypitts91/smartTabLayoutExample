a8a5bca80d9f8234158f39ca5480159bf8386c04
Kelly Sharma <sharmak@oxygenforbusiness.com>
2018-06-20 11:50:38 +1200

    fixed layout being inflated

--- onBoarding/app/src/main/java/com/example/sharmak/onboarding/OnboardingFragment2.java
+++ onBoarding/app/src/main/java/com/example/sharmak/onboarding/OnboardingFragment2.java
@@ -13,6 +13,6 @@ public class OnboardingFragment2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_onboarding_fragment1, container, false);
+        return inflater.inflate(R.layout.fragment_onboarding_fragment2, container, false);
     }
 }
